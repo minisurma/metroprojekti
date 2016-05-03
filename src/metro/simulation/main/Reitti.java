baa3cd7c5daa7137e2a8e9ce369ac59468adf7f9
xxqueeenxx <samivaris95@gmail.com>
2016-05-03 12:37:47 +0300

    Create Reitti.java

---
+++ src/metro/simulation/main/Reitti.java
@@ -0,0 +1,15 @@
+package com.metro.objects;
+
+public class Reitti{
+
+	private Asema begin, destination;
+	
+	private int sijainti;
+	
+	public Reitti(Asema begin, Asema destination){
+		this.begin = begin;
+		this.destination = destination;
+	}
+	
+	
+}
