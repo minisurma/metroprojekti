0936b7eb6d337d16a545f6729bd75fbe318197c5
minisurma <ppxxy@hotmail.com>
2016-05-12 23:13:57 +0300

    Update Asema.java

--- src/metro/simulation/objects/Asema.java
+++ src/metro/simulation/objects/Asema.java
@@ -1,9 +1,17 @@
-package com.metro.objects;
+package metro.simulation.objects;
+
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import metro.simulation.main.Main;
 
 public class Asema {
 
 	private int id;
 	private String nimi;
+	private Point east, west;
 	
 	public static final Asema Mellunmaki = new Asema(0, "Mellunmäki");
 	public static final Asema Kontula = new Asema(1, "Kontula");
@@ -23,6 +31,11 @@ public class Asema {
 	public static final Asema Rastila = new Asema(15, "Rastila");
 	public static final Asema Vuosaari = new Asema(16, "Vuosaari");
 	
+	public static final Asema[] asemat = new Asema[]{
+		Mellunmaki, Kontula, Myllypuro, Itakeskus, Siilitie, Herttoniemi, Kulosaari, Kalasatama, Sornainen, 
+		Hakaniemi, Helsingin_Yliopisto, Rautatieasema, Kamppi, Ruoholahti, Puotila, Rastila, Vuosaari
+	};
+	
 	public Asema(){
 		
 	}
@@ -30,5 +43,73 @@ public class Asema {
 	public Asema(int id, String nimi){
 		this.id = id;
 		this.nimi = nimi;
+		this.east = getEastPoint();
+		this.west = getWestPoint();
 	}
+
+	private Point getEastPoint() {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/resource/other/asemat.txt")));
+			for(String line = reader.readLine(); line != null; line = reader.readLine()){
+				if(line.startsWith(String.valueOf(this.id +" "))){
+					int x = Integer.parseInt((line.split(", ")[0]).split(" ")[1]);
+					int y = Integer.parseInt((line.split(", ")[1]).split(" ")[0]);
+					return new Point(x, y);
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private Point getWestPoint() {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(Main.class.getResourceAsStream("/resource/other/asemat.txt")));
+			for(String line = reader.readLine(); line != null; line = reader.readLine()){
+				if(line.startsWith(String.valueOf(this.id +" "))){
+					int x = Integer.parseInt((line.split(", ")[1]).split(" ")[1]);
+					int y = Integer.parseInt((line.split(", ")[2]).split(" ")[0]);
+					return new Point(x, y);
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public boolean sijaitsee(Point point) {
+		if(this.east.equals(point) || this.west.equals(point)){
+			return true;
+		}
+		return false;
+	}
+	
+	public Point getLocation(){
+		switch(id){
+		case 0:
+			return west;
+		case 1:
+			return east;
+		case 2:
+			return west;
+		case 3:
+			return east;
+		case 13:
+			return east;
+		case 16: 
+			return west;
+		}
+		return null;
+	}
+
+	public String getName() {
+		return this.nimi;
+	}
+	
 }
